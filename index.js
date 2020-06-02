be2624efb3b5be4a57f438e3b26a84b345251600
rookiedraptor <avwunuferookietarvenir@gmail.com>
2020-06-02 16:26:40 +0000

    fixed dependency error

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose.connection.once("open", ()=>{
 }).on("error", ()=>{
     console.log("There has been an error")
 })
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
     console.log("Now listening on port"+ PORT)
 })
